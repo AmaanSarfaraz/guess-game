338c1a9d1f4381c603d2c81f7e39933b6cc11089
AMAAN SARFARAZ <Sarfarazmohammad369@gmail.com>
2023-06-25 23:41:10 +0530

    initial commit

---
+++ script1.js
@@ -0,0 +1,47 @@
+"use strict";
+let SecretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+let highScore = 0;
+
+document.getElementById("check1").addEventListener("click", function () {
+  let guess = Number(document.getElementById("guess").value);
+  if (!guess) {
+    document.getElementById("message").textContent = "no number";
+  } else if (guess === SecretNumber) {
+    document.getElementById("message").textContent = "congratulations";
+    document.getElementById("toguess").textContent = SecretNumber;
+    document.querySelector("body").style.backgroundColor = "green";
+    document.getElementById("toguess").style.width = "300px";
+    if (score > highScore) {
+      highScore = score;
+      document.getElementById("high").textContent = highScore;
+    }
+  } else if (guess > SecretNumber) {
+    if (score > 1) {
+      document.getElementById("message").textContent = "too high try again";
+      score = score - 1; //or we can write score--
+      document.getElementById("score").textContent = score;
+    } else {
+      document.getElementById("message").textContent = "You Lose";
+    }
+  } else if (guess < SecretNumber) {
+    if (score > 1) {
+      document.getElementById("message").textContent = "too low try again";
+      score--;
+      document.getElementById("score").textContent = score;
+    } else {
+      document.getElementById("message").textContent = "You Lose";
+    }
+  }
+});
+document.getElementById("again").addEventListener("click", function () {
+  SecretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.getElementById("message").textContent = "Start Guessing...";
+  document.getElementById("score").textContent = score;
+  document.getElementById("toguess").textContent = "?";
+  document.getElementById("guess").value = "";
+  document.querySelector("body").style.backgroundColor = "black";
+  document.getElementById("toguess").style.width = "150px";
+});
